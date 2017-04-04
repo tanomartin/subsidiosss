1d5d49b4cab5fb6c5807a797c42a4372559c2ef5
tanomartin <tanomartin05@gmail.com>
2017-04-04 12:17:42 -0300

    subida menu subsidios

---
+++ menu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>.: Menú Subsidio S.S.S. :.</title>
+
+<script language="javascript">
+function abrirModulo(dire, titulo) {
+	a= window.open(dire,titulo,
+	"toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=800, height=500, top=10, left=10");
+}
+</script>
+
+</head>
+<body bgcolor="#CCCCCC">
+<div align="center">
+  <p><b>Subsidio S.S.S.</b></p>
+  <table width="600" border="1" style="text-align: center">
+    <tr>
+      <td> 
+        <p>Cronograma</p>
+        <p><a href="javascript:abrirModulo('acuerdos/menuAcuerdos.php','AcuerdosOspim')"><img src="img/cronograma.png" width="90" height="90" border="0"/></a></p>
+	  </td>
+      <td>
+	    <p>Presentacion</p>
+	    <p><a href="javascript:abrirModulo('../comun/empresas/menuEmpresa.php?origen=ospim','EmpresasOspim')"><img src="img/presentacion.png" width="90" height="90" border="0" alt="enviar"/></a></p>
+	  </td>
+      <td>
+	    <p>Errores S.S.S.</p>
+	    <p><a href="javascript:abrirModulo('afiliados/menuAfiliados.php','AfiliadosOspim')"><img src="img/error.png" width="90" height="90" border="0" /></a></p>
+      </div>
+	  </td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
