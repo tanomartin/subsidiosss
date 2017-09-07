6cb0a877c093acd735895b875071a107efa93ecd
tanomartin <tanomartin05@gmail.com>
2017-09-07 09:48:57 -0300

    control de acceso

--- verificador.php
+++ verificador.php
@@ -4,12 +4,16 @@ $usuario = $datos[0];
 $clave = $datos[1];
 $host = $_SERVER['SERVER_NAME'];
 session_start();
-if ($_SESSION['usuario'] == $usuario) {
-	header ('location:index.php?error=2');	
-}
+
+if (isset($_SESSION['usuario'])) {
+	if ($_SESSION['usuario'] == $usuario) {
+		header ('location:index.php?error=2');	
+	}
+} 
+
 $dbusuario =  mysql_connect($host,$usuario, $clave);
 if (!$dbusuario or $usuario == "" or $clave == "") {
-  	header ('location:index.php?error=1');	
+	 	header ('location:index.php?error=1');	
 } else {
 	$_SESSION['host']= $host;
 	$_SESSION['usuario'] = $usuario;
@@ -19,6 +23,7 @@ if (!$dbusuario or $usuario == "" or $clave == "") {
 	$_SESSION['ultimoAcceso'] = date("Y-n-j H:i:s");
 	header ('location:menu.php');
 }
+
 ?>
 
 
