66db6e7115469e546483ba299ba048cffe4db202
tanomartin <tanomartin05@gmail.com>
2017-09-07 09:42:31 -0300

    cambio verificador

--- verificador.php
+++ verificador.php
@@ -4,23 +4,12 @@ $usuario = $datos[0];
 $clave = $datos[1];
 $host = $_SERVER['SERVER_NAME'];
 session_start();
-
-//control solo para sistemas//
-/* if ($usuario != "sistemas") {
-	header ('location:mantenimiento.php');
-	exit(0);
-}*/
-//**************************//
-
 if ($_SESSION['usuario'] == $usuario) {
 	header ('location:index.php?error=2');	
-	exit(0);
 }
-
 $dbusuario =  mysql_connect($host,$usuario, $clave);
 if (!$dbusuario or $usuario == "" or $clave == "") {
-  	header ('location:index.php?error=1');
-  	exit(0);
+  	header ('location:index.php?error=1');	
 } else {
 	$_SESSION['host']= $host;
 	$_SESSION['usuario'] = $usuario;
