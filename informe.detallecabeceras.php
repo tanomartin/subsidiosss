ccb6e9576c07643d6f0ccb979a96892c13de0a7a
tanomartin <tanomartin05@gmail.com>
2018-02-08 13:59:35 -0300

    agregado datos totalizadores

--- informe.detallecabeceras.php
+++ informe.detallecabeceras.php
@@ -29,7 +29,22 @@ LEFT JOIN intepresentacionintegral on p.id = intepresentacionintegral.id
 LEFT JOIN intepresentacionsubsidio on p.id = intepresentacionsubsidio.id
 LEFT JOIN interendicioncontrol on p.id = interendicioncontrol.idpresentacion
 WHERE p.fechacancelacion is null ORDER BY p.id DESC";
-$resPresentacion = mysql_query($sqlPresentacion);?>
+$resPresentacion = mysql_query($sqlPresentacion);
+
+
+$sqlPromedio = "SELECT
+count(*) as cantidad,
+sum(importesolicitado) as totsoli, 
+sum(importeliquidado) as totliqui,
+sum(importesolicitado) - sum(importeliquidado) as dif,
+sum(importesolicitado) / count(*) as promsol,
+sum(importeliquidado) / count(*) as promliq,
+(sum(importesolicitado) - sum(importeliquidado)) / count(*) as promdif,
+(sum(importeliquidado) / sum(importesolicitado)) * 100 as porcentaje
+FROM interendicioncontrol";
+$resPromedio = mysql_query($sqlPromedio);
+$rowPromedio = mysql_fetch_array($resPromedio)
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -47,6 +62,37 @@ $resPresentacion = mysql_query($sqlPresentacion);?>
 <body bgcolor="#CCCCCC">
 	<div align="center">
 	<p><input type="button" name="volver" value="Volver" onClick="location.href = 'informes.php'" /></p>
+	<h2>Datos Totalizadores de Presentaciones</h2>
+	
+	<div class="grilla" style="width: 80%">
+		<table>
+			<thead>
+				<tr>
+					<th>Can. Pres.</th>
+					<th>Tot. Solicitado</th>
+					<th>Tot. Liquidado</th>
+					<th>Tot. Diferencia</th>
+					<th>Prom. Solicitado</th>
+					<th>Prom. Liquidado</th>
+					<th>Prom. Diferencia</th>
+					<th>%. Recuperado</th>
+				</tr>
+			</thead>
+				<tr>
+					<td><?php echo $rowPromedio['cantidad']?></td>
+					<td><?php echo "$ ".number_format($rowPromedio['totsoli'],"2",",",".") ?></td>
+					<td><?php echo "$ ".number_format($rowPromedio['totliqui'],"2",",",".") ?></td>
+					<td><?php echo "$ ".number_format($rowPromedio['dif'],"2",",",".") ?></td>
+					<td><?php echo "$ ".number_format($rowPromedio['promsol'],"2",",",".") ?></td>
+					<td><?php echo "$ ".number_format($rowPromedio['promliq'],"2",",",".") ?></td>
+					<td><?php echo "$ ".number_format($rowPromedio['promdif'],"2",",",".") ?></td>
+					<td><?php echo number_format($rowPromedio['porcentaje'],"2",",","")." %" ?></td>
+				</tr>
+			<tbody>
+			</tbody>
+		</table>
+	</div>
+	
 	<h2>Detalle Presentaciones Finalizadas</h2>
 
 <?php while ($rowPresentacion = mysql_fetch_array($resPresentacion)) { ?>
