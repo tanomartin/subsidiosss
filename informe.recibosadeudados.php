c88006e0e40439e80af8208101a3c4e397dda125
tanomartin <tanomartin05@gmail.com>
2018-06-05 12:04:16 -0300

    subida informes adeudados.

--- informe.recibosadeudados.php
+++ informe.recibosadeudados.php
@@ -3,45 +3,65 @@ include_once 'include/conector.php';
 set_time_limit(0);
 
 $sqlRecibos = "SELECT
-f.idpresentacion,
-c.carpeta,
-c.periodo,
-prestadores.nombre,
-f.cuit,
-f.nrocominterno,
-f.cuil,
-titulares.codidelega as deletitu, 
-titufami.codidelega as delefami,
-m.descripcion,
-f.nrocomprobante,
-f.periodo,
-f.impcomprobante,
-pc.nrotransferencia,
-DATE_FORMAT(pc.fechatransferencia,'%d-%m-%Y') as fechatrasferencia,
-prestadores.email1,
-prestadores.email2,
-prestadores.ddn1,
-prestadores.telefono1,
-prestadores.ddn2,
-prestadores.telefono2
-FROM intepagosdetalle p, intepagoscabecera pc, intepresentacion pre, intecronograma c, tipocomprobante m, intepresentaciondetalle f
+	f.idpresentacion,
+	c.carpeta,
+	c.periodo as periodocarpeta,
+	prestadores.nombre,
+	f.cuit,
+	f.nrocominterno,
+	f.cuil,
+	CASE
+     WHEN (titulares.codidelega is not null) THEN titulares.codidelega
+ 		WHEN (titularesdebaja.codidelega is not null) THEN titularesdebaja.codidelega
+ 		WHEN (titufami.codidelega is not null) THEN titufami.codidelega
+ 		WHEN (titubajafami.codidelega is not null) THEN titubajafami.codidelega
+ 		WHEN (titufamibaja.codidelega is not null) THEN titufamibaja.codidelega
+ 		WHEN (titubajafamibaja.codidelega is not null) THEN titubajafamibaja.codidelega
+	END as codidelega,
+	m.descripcion,
+	f.nrocomprobante,
+	f.periodo,
+	f.impcomprobante,
+	pc.nrotransferencia,
+	DATE_FORMAT(pc.fechatransferencia,'%d-%m-%Y') as fechatrasferencia,
+	prestadores.email1,
+	prestadores.email2,
+	prestadores.ddn1,
+	prestadores.telefono1,
+	prestadores.ddn2,
+	prestadores.telefono2
+FROM 
+	intepagosdetalle p, 
+	intepagoscabecera pc, 
+	intepresentacion pre, 
+	intecronograma c, 
+	tipocomprobante m, 
+	intepresentaciondetalle f
+		
 LEFT JOIN titulares on f.cuil = titulares.cuil
+LEFT JOIN titularesdebaja on f.cuil = titularesdebaja.cuil
 LEFT JOIN familiares on f.cuil = familiares.cuil
-LEFT JOIN titulares titufami on  familiares.nroafiliado = titufami.nroafiliado
-LEFT JOIN prestadores on f.cuit = prestadores.cuit
+LEFT JOIN titulares titufami on familiares.nroafiliado = titufami.nroafiliado
+LEFT JOIN titularesdebaja titubajafami on familiares.nroafiliado = titubajafami.nroafiliado
+LEFT JOIN familiaresdebaja on f.cuil = familiaresdebaja.cuil
+LEFT JOIN titulares titufamibaja on familiaresdebaja.nroafiliado = titufamibaja.nroafiliado
+LEFT JOIN titularesdebaja titubajafamibaja on familiaresdebaja.nroafiliado = titubajafamibaja.nroafiliado
+		
+LEFT JOIN prestadores on f.cuit = prestadores.cuit	
 WHERE
-  p.recibo = '' and
-  p.nrocominterno = f.nrocominterno and
-  p.idpresentacion = f.idpresentacion and 
-  pc.idpresentacion = p.idpresentacion and
-  codpractica not in (97,98,99) and
-  f.impsolicitadosubsidio is not null and
-  f.impmontosubsidio is not null and
-  f.idpresentacion = pre.id and
-  pre.idcronograma = c.id and
-  f.tipocomprobante = m.id
-group by f.cuit, f.nrocomprobante
-order by f.idpresentacion, f.cuit";
+	p.recibo = '' and
+	p.nrocominterno = f.nrocominterno and
+	p.idpresentacion = f.idpresentacion and 
+	p.idpresentacion = pc.idpresentacion and 
+	p.nroordenpago = pc.nroordenpago and
+	f.codpractica not in (97,98,99) and
+	f.impsolicitadosubsidio is not null and
+	f.impmontosubsidio is not null and
+	f.idpresentacion = pre.id and
+	pre.idcronograma = c.id and
+	f.tipocomprobante = m.id
+GROUP BY f.cuit, f.nrocomprobante
+ORDER BY f.idpresentacion, f.cuit";
 $resRecibos = mysql_query($sqlRecibos);
 
 $today = date("m-d-y");
@@ -80,12 +100,12 @@ header("Content-Disposition: attachment; filename=$file");
 				<tr>
 			 		<td><?php echo $rowRecibo['idpresentacion'] ?></td>
 			 		<td><?php echo $rowRecibo['carpeta'] ?></td>
-			 		<td><?php echo $rowRecibo['periodo'] ?></td>
+			 		<td><?php echo $rowRecibo['periodocarpeta'] ?></td>
 			 		<td><?php echo $rowRecibo['nombre'] ?></td>
 			 		<td><?php echo $rowRecibo['cuit'] ?></td>
 			 		<td><?php echo $rowRecibo['nrocominterno'] ?></td>
 			 		<td><?php echo $rowRecibo['cuil'] ?></td>
-			 		<td><?php echo $rowRecibo['deletitu']." ".$rowRecibo['delefami'] ?></td>
+			 		<td><?php echo $rowRecibo['codidelega'] ?></td>
 			 		<td><?php echo $rowRecibo['descripcion'] ?></td>
 			 		<td><?php echo $rowRecibo['periodo'] ?></td>
 			 		<td><?php echo $rowRecibo['nrocomprobante'] ?></td>
