1b1d8ca2d1ca17e6af2214b42fbe2c2c9965845d
tanomartin <tanomartin05@gmail.com>
2019-01-23 12:35:04 -0300

    cambio de color carpeta actual en cronograma

--- cronograma.php
+++ cronograma.php
@@ -3,6 +3,12 @@ include_once 'include/conector.php';
 $sqlCronograma = "SELECT c.*,DATE_FORMAT(c.fechacierre,'%d/%m/%Y') as fechacierre FROM intecronograma c ORDER By carpeta DESC";
 $resCronograma = mysql_query($sqlCronograma);
 
+$today = date("Y-m-d");
+$carpeta = date("Ym");
+$sqlCarpetaActual = "SELECT id FROM intecronograma i where fechacierre >= '$today' and carpeta < '$carpeta'";
+$resCarpetaActual = mysql_query($sqlCarpetaActual);
+$rowCarpetaActual = mysql_fetch_array($resCarpetaActual);
+$idCarpetaActual = $rowCarpetaActual['id'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -27,12 +33,14 @@ $resCronograma = mysql_query($sqlCronograma);
 		 		</tr>
 		 	</thead>
 		 	<tbody>
-		<?php while ($rowCronograma = mysql_fetch_array($resCronograma)) {  ?>
+		<?php while ($rowCronograma = mysql_fetch_array($resCronograma)) { 
+				$color = "";
+				if ($idCarpetaActual == $rowCronograma['id']) { $color = 'style = "color: red"'; }?>
 				<tr>
-					<td><?php echo $rowCronograma['periodo'] ?></td>
-					<td><?php echo $rowCronograma['carpeta'] ?></td>
-					<td><?php echo $rowCronograma['fechacierre'] ?></td>
-					<td><?php echo $rowCronograma['periodosincluidos'] ?></td>
+					<td <?php echo $color ?>><?php echo $rowCronograma['periodo'] ?></td>
+					<td <?php echo $color ?>><?php echo $rowCronograma['carpeta'] ?></td>
+					<td <?php echo $color ?>><?php echo $rowCronograma['fechacierre'] ?></td>
+					<td <?php echo $color ?>><?php echo $rowCronograma['periodosincluidos'] ?></td>
 				</tr>
 		<?php } ?>
 		  	</tbody>
