992f2c1b81f4ddc3e7b08993ab1be96c91fce3a7
tanomartin <tanomartin05@gmail.com>
2019-04-26 09:03:28 -0300

    ordenamiento de errores

--- presentacion.erroresformato.php
+++ presentacion.erroresformato.php
@@ -25,7 +25,7 @@ while ($rowCantidad = mysql_fetch_array($resCantidad)) {
 		}
 	}
 }
-
+ksort($arrayCantidad);
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
