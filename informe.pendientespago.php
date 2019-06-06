11c8028116bdf312123798e1f9fb2e8cdb697209
tanomartin <tanomartin05@gmail.com>
2019-06-06 12:43:32 -0300

    Cambio informe de pagos

--- informe.pendientespago.php
+++ informe.pendientespago.php
@@ -1,15 +1,41 @@
 <?php include_once 'include/conector.php';
 set_time_limit(0);
 
-$sqlPendientes = "SELECT inteinterbanking.*,
-						 DATE_FORMAT(inteinterbanking.fechaenvio, '%d-%m-%Y') as fechaenvio,
-						 madera.prestadoresauxiliar.cbu,
-						 DATE_FORMAT(madera.prestadoresauxiliar.fechainterbanking, '%d-%m-%Y') as fechainterbanking
-				  FROM inteinterbanking
-				  LEFT JOIN madera.prestadores on inteinterbanking.cuit = madera.prestadores.cuit
-				  LEFT JOIN madera.prestadoresauxiliar on madera.prestadores.codigoprestador = madera.prestadoresauxiliar.codigoprestador
-				  WHERE idpago is null and nopagar = 0 ORDER BY idpresentacion";
+$sqlPresPagos = "SELECT idpresentacion FROM intepagosdetalle GROUP BY idpresentacion";
+$resPresPagos = mysql_query($sqlPresPagos);
+$whereInPres = "(";
+while ($rowPresPagos = mysql_fetch_assoc($resPresPagos)) {
+	$whereInPres .= $rowPresPagos['idpresentacion'].",";
+}
+$whereInPres = substr($whereInPres, 0, -1);
+$whereInPres .= ")";
+
+$sqlPendientes = "SELECT * 
+					FROM intepresentaciondetalle i, interendicioncontrol r
+					WHERE
+						r.idpresentacion in $whereInPres AND
+						i.idpresentacion = r.idpresentacion AND 
+						i.codpractica not in (97,98,99) AND
+						i.deverrorintegral IS NULL AND 
+						i.tipoarchivo != 'DB' AND
+						i.nrocominterno NOT IN (select nrocominterno FROM intepagosdetalle) 
+					ORDER BY i.idpresentacion, i.nrocominterno";
 $resPendientes = mysql_query($sqlPendientes);
+
+$arrayReversiones = array();
+$sqlReversionesFuturas = "SELECT d.nrocominterno, d.idpresentacion
+							FROM intepresentaciondetalle d, intepresentacion p, interendicioncontrol r
+							WHERE d.tipoarchivo = 'DB' AND p.id = r.idpresentacion AND
+							d.idpresentacion = p.id AND p.fechacancelacion is null";
+$resReversionesFuturas = mysql_query($sqlReversionesFuturas);
+$canReversionesFuturas = mysql_num_rows($resReversionesFuturas);
+if ($canReversionesFuturas > 0) {
+	while ($rowReversionesFuturas = mysql_fetch_assoc($resReversionesFuturas)) {
+		$arrayReversiones[$rowReversionesFuturas['nrocominterno']] = $rowReversionesFuturas['idpresentacion'];
+	}
+}
+
+
 
 $today = date("m-d-y");
 $file= "Pagos Pendientes al $today.xls";
@@ -22,35 +48,33 @@ header("Content-Disposition: attachment; filename=$file");
 		<table border="1">
 			<thead>
 				<th>Pres</th>
+				<th>Com. Int.</th>
 				<th>C.U.I.T.</th>
-				<th>C.B.U.</th>
-				<th>Interbanking</th>
 				<th>$ Comp.</th>
 				<th>$ Deb.</th>
 				<th>$ No Int</th>
 				<th>$ Soli.</th>
 				<th>$ O.S.</th>
 				<th>$ S.S.S</th>
-				<th>$ Ret.</th>
 				<th>$ A Pagar</th>
 			</thead>
 			<tbody>
-			<?php while ($rowPendientes = mysql_fetch_assoc($resPendientes)) { ?>
+			<?php while ($rowPendientes = mysql_fetch_assoc($resPendientes)) {
+					if (!array_key_exists ($rowPendientes['nrocominterno'], $arrayReversiones)) { ?>
 					<tr>
 						<td><?php echo $rowPendientes['idpresentacion'] ?></td>
+						<td><?php echo $rowPendientes['nrocominterno'] ?></td>
 						<td><?php echo $rowPendientes['cuit'] ?></td>
-						<td><?php if ($rowPendientes['cbu'] != null) { echo "'".$rowPendientes['cbu']."'"; } ?></td>
-						<td><?php echo $rowPendientes['fechainterbanking'] ?></td>
 						<td><?php echo number_format($rowPendientes['impcomprobanteintegral'],2,",",".") ?></td>
 						<td><?php echo number_format($rowPendientes['impdebito'],2,",",".") ?></td>
 						<td><?php echo number_format($rowPendientes['impnointe'],2,",",".") ?></td>
 						<td><?php echo number_format($rowPendientes['impsolicitadosubsidio'],2,",",".") ?></td>					
-						<td><?php echo number_format($rowPendientes['impobrasocial'],2,",",".") ?></td>
+						<td><?php echo number_format($rowPendientes['impcomprobanteintegral'] - $rowPendientes['impdebito'] - $rowPendientes['impmontosubsidio'],2,",",".") ?></td>
 						<td><?php echo number_format($rowPendientes['impmontosubsidio'],2,",",".") ?></td>				
-						<td><?php echo number_format($rowPendientes['impretencion'],2,",",".") ?></td>
-						<td><?php echo number_format($rowPendientes['impapagar'],2,",",".") ?></td>
+						<td><?php echo number_format($rowPendientes['impcomprobanteintegral'] - $rowPendientes['impdebito'],2,",",".") ?></td>
 					</tr>
-			<?php } ?>
+			<?php	}
+				 } ?>
 			</tbody>
 		</table>
 	</div>
